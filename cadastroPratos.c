b71739aa66fdb723bd5a2f091ec73c968c1d5e4f
Gabriel Ceron Bianchi <142252182+cbGabriel9@users.noreply.github.com>
2024-06-09 19:28:06 -0300

    Adicionei a função de limpar buffer e coloquei funcionar os código em conjunto

--- cadastroPratos.c
+++ cadastroPratos.c
@@ -3,32 +3,38 @@
 #include <string.h>
 #include <stdbool.h>
 
-char nome[10][50] = {"Ajuste", "Sorvete", 0, 0, 0, 0, 0, 0, 0, 0};          // Inicializo o vetor com valores prédefinidos
-char descricao[10][300] = {"Ajuste", "Bem gelado", 0, 0, 0, 0, 0, 0, 0, 0}; // Ajuste é para começar a mostrar os pratos na posição 1
+char pratos[10][50] = {"Ajuste", "Sorvete", "", "", "", "", "", "", "", ""};          // Inicializo o vetor com valores prédefinidos
+char descricao[10][300] = {"Ajuste", "Bem gelado", "", "", "", "", "", "", "", ""}; // Ajuste é para começar a mostrar os pratos na posição 1
 float preco[10] = {1, 19.90, 0, 0, 0, 0, 0, 0, 0, 0};
 
-void CadastroPratos(void)
-{    
+void limpaBufferDeEntrada() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
 
+void CadastroPratos(void)
+{
+
+    
     bool repeat = true;
     int i, resp;
 
     while (repeat == true) // Para repetir ou voltar o menu, escolhendo no final do código
     {
-
+        
         for (i = 1; i < 10; i++)
         { // Laço de repetição para adicionar um prato na lista
-            if (strlen(nome[i]) == 0)
-            { // Verifica se o índice está vazio para ser preenchido, evitando sobrescrever algum prato
-
+            if (strlen(pratos[i]) == 0) // Verifica se o índice está vazio para ser preenchido, evitando sobrescrever algum prato
+            { 
                 printf("Adicione o nome do prato: ");
-                fgets(nome[i], 50, stdin); // Lê uma linha inteira da entrada, incluindo o caractere de nova linha, permitindo escrever mais de uma palavra
+                limpaBufferDeEntrada();
+                fgets(pratos[i], 50, stdin); // Lê uma linha inteira da entrada, incluindo o caractere de nova linha, permitindo escrever mais de uma palavra
 
-                size_t len = strlen(nome[i]); // Armazena o tamanho da string
+                size_t len = strlen(pratos[i]); // Armazena o tamanho da string
 
-                if (len > 0 && nome[i][len - 1] == '\n') // Remove o caractere de nova linha do final da string, se presente
+                if (len > 0 && pratos[i][len - 1] == '\n') // Remove o caractere de nova linha do final da string, se presente
                 {
-                    nome[i][len - 1] = '\0';
+                    pratos[i][len - 1] = '\0';
                 }
 
                 break; // Comando utilizado para adicionar apenas um prato por vez
@@ -68,9 +74,9 @@ void CadastroPratos(void)
 
         for (i = 1; i < 10; i++) // Mostra todos os pratos disponíveis
         {
-            if (strlen(nome[i]) != 0) // Verifica se naquela posição possui caracter, se possuir mostra o conteúdo, evitando mostrar campos vazios
+            if (strlen(pratos[i]) != 0) // Verifica se naquela posição possui caracter, se possuir mostra o conteúdo, evitando mostrar campos vazios
             {
-                printf("\nPrato numero %d: %s", i, nome[i]);
+                printf("\nPrato numero %d: %s", i, pratos[i]);
                 printf("\nDescricao %d: %s", i, descricao[i]);
                 printf("\nPreco %d: %.2f", i, preco[i]);
             }
@@ -84,7 +90,6 @@ void CadastroPratos(void)
             if (resp == 1)
             {
                 repeat = true;
-                while (getchar() != '\n')
                     ; // Consome o caractere de nova linha para não influenciar no próximo fgets
             }
             else if (resp == 2)
@@ -97,11 +102,5 @@ void CadastroPratos(void)
             }
         } while (resp != 1 && resp != 2);
     }
-    // printf("%zu\n", strlen(nome[i])); // É utilizado para contar quantos caracteres tem naquela posição da array
+    // printf("%zu\n", strlen(pratos[i])); // É utilizado para contar quantos caracteres tem naquela posição da array
 }
-
-// int main()
-// {
-//     cadastroPratos(); // Chama a função cadastroPratos
-//     return 0;
-// }
