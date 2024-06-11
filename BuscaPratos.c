40789e7d216145c7f5718c230b8be678aa9bce0d
Gabriel Ceron Bianchi <142252182+cbGabriel9@users.noreply.github.com>
2024-06-10 21:48:46 -0300

    Ajuste final - Comentários

--- BuscaPratos.c
+++ BuscaPratos.c
@@ -1,201 +1,76 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <locale.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdio.h> //Biblioteca de entrada e saída simples
+#include <stdlib.h> //Biblioteca de funções padrões da linguagem C (strlen, strstr, strcopy e etc...)
+#include <stdbool.h> //Biblioteca que permite utilizar o tipo booleano das variáveis
+//Todos os "#include" abaixo com os arquivos .c permitem nós fazermos a utilização da função de cada arquivo 
+#include "BuscaPratos.c"
+#include "ListaPratos.c"
+#include "cadastroPratos.c" 
+#include "removerPratos.c"
+#include "registraPedido.c"
 
-// Função para converter uma string para minúsculas
-void Str_minuscula(char *strMinuscula, const char *strOriginal)
+int main ()
 {
-    while (*strOriginal)
-    {
-        *strMinuscula = tolower((unsigned char)*strOriginal);
-        strMinuscula++;
-        strOriginal++;
-    }
-    *strMinuscula = '\0'; // Finaliza a string com o caractere nulo
-}
+    setlocale(LC_ALL, "Portuguese");
 
-void BuscaPorNome(void)
-{
-    extern char nome[10][50];
-    extern char descricao[10][300];
-    extern float preco[10];
+    void CadastroPratos(void);
+    void RemoverPratos(void);
+    void BuscaPratos(void);
+    void ListaPratos(void);
+    void RegistraPedido(void);
 
-    char busca[100];
-
-    printf("Digite o nome que deseja encontrar: ");
-    scanf("%s", busca);
-
-    int tamanhoArray = (sizeof(nome) / sizeof(nome[0]));
-    bool pratoEncontrado = false;
-
-    for (int i = 0; i < tamanhoArray; i++)
-    {
-        char pratosMinusculo[100];
-        char buscaMinuscula[100];
-
-        Str_minuscula(pratosMinusculo, nome[i]);
-        Str_minuscula(buscaMinuscula, busca);
-
-        if (strstr(pratosMinusculo, buscaMinuscula) != NULL)
-        {
-            printf("Pratos encontrados:\n");
-            printf("Nome: %s\n", nome[i]);
-            printf("Descricao: %s\n", descricao[i]);
-            printf("Preco: %.2f\n", preco[i]);
-            printf("----------------------------\n");
-            pratoEncontrado = true;
-        }
-    }
-
-    if (pratoEncontrado == false)
-        printf("Prato nao encontrado!\n");
 
     bool loop = true;
-    do
-    {
-        printf("O que deseja fazer?\n");
-        printf("1 - Pesquisar outro prato\n");
-        printf("2 - Sair\n");
-
-        int decisao;
-        scanf("%d", &decisao);
-        switch (decisao)
-        {
-        case 1:
-            BuscaPorNome();
-
-            loop = false;
-            break;
-
-        case 2:
-            printf("Voce escolheu sair.\n");
-            printf("\n");
-
-            loop = false;
-            break;
-
-        default:
-            printf("Opcao invalida, tente novamente.\n");
-            printf("\n");
-
-            loop = true;
-            break;
-        }
-    } while (loop);
-}
-
-void BuscaPorDescricao(void)
-{
-    extern char nome[10][50];
-    extern char descricao[10][300];
-    extern float preco[10];
-
-    char busca[100];
-
-    printf("Digite a descricao que deseja encontrar: ");
-    scanf("%s", busca);
-
-    int tamanhoArray = (sizeof(descricao) / sizeof(descricao[0]));
-    bool pratoEncontrado = false;
-
-    for (int i = 0; i < tamanhoArray; i++)
-    {
-        char descricaoMinuscula[100];
-        char buscaMinuscula[100];
-
-        Str_minuscula(descricaoMinuscula, descricao[i]);
-        Str_minuscula(buscaMinuscula, busca);
-
-        if (strstr(descricaoMinuscula, buscaMinuscula) != NULL)
-        {
-            printf("Pratos encontrados:\n");
-            printf("Nome: %s\n", nome[i]);
-            printf("Descricao: %s\n", descricao[i]);
-            printf("Preco: %.2f\n", preco[i]);
-            printf("----------------------------\n");
-            pratoEncontrado = true;
-        }
-    }
-    if (pratoEncontrado == false)
-        printf("Prato nao encontrado!\n");
-
-    bool loop = true;
-    do
-    {
-        printf("O que deseja fazer?\n");
-        printf("1 - Pesquisar outro prato\n");
-        printf("2 - Sair\n");
-
-        int decisao;
-        scanf("%d", &decisao);
-
-        switch (decisao)
-        {
-        case 1:
-            BuscaPorDescricao();
-
-            loop = false;
-            break;
-
-        case 2:
-            printf("Voce escolheu sair.\n");
-            printf("\n");
-
-            loop = false;
-            break;
-
-        default:
-            printf("Opcao invalida, tente novamente.\n");
-            printf("\n");
-
-            loop = true;
-            break;
-        }
-    } while (loop);
-}
-
-void BuscaPratos(void)
-{
-    printf("\n");
-    printf("*************\n");
-    printf("MODO DE BUSCA\n");
-    printf("*************\n");
-    printf("\n");
-
-    int aux = -1;
-
-    do
-    {
-        printf("Deseja buscar por nome ou descricao?\n");
-        printf("1 - Nome\n");
-        printf("2 - Descricao\n");
+    do {
+        int decisao = 0;
+        //Interface inicial do programa
+        printf("=================================================================");
+        printf("\n=============== Gabriel e Thiago - Restaurante ==================");
+        printf("\n=================================================================");
+        printf("\nSeja bem vindo ao nosso sistema! O que deseja fazer?");
+        printf("\n=================================================================\n");
+        printf("1 - Cadastro de Pratos\n");
+        printf("2 - Remocao de Pratos\n");
+        printf("3 - Busca de Pratos\n");
+        printf("4 - Listagem de Pratos\n");
+        printf("5 - Registrar um pedido\n");
         printf("0 - SAIR\n");
 
-        scanf("%d", &aux);
+        scanf ("%d", &decisao);
 
-        switch (aux)
+        //É feito um switch case para chamar a função de cada arquivo de acordo com o número digitado pelo usuário anteriormente
+        switch (decisao)
         {
-        case 1:
-            BuscaPorNome();
-            aux = 0;
+            case 1:
+                CadastroPratos();
+            break;
+        
+            case 2:
+                RemoverPratos();
             break;
 
-        case 2:
-            BuscaPorDescricao();
-            aux = 0;
+            case 3:
+                BuscaPratos();
             break;
 
-        case 0:
-            aux = 0;
+            case 4:
+                ListaPratos();
             break;
 
-        default:
-            printf("Opcao invalida, tente novamente.\n");
-            aux = -1;
+            case 5:
+                RegistraPedido();
+            break;
+
+            case 0:
+                loop = false;
+            break;
+        
+            
+            default:
+                printf("Opcao escolhida invalida! Por favor tente novamente");
+                loop = true;
             break;
         }
-    } while (aux == -1);
+    } while (loop);
+
+    return 0;
 }
