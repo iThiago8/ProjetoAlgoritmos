53a94b099ad9bbad25432b57b39e0ae39bc55e06
Gabriel Ceron Bianchi <142252182+cbGabriel9@users.noreply.github.com>
2024-06-10 21:49:17 -0300

    Ajuste final - Comentários

--- ListaPratos.c
+++ ListaPratos.c
@@ -5,11 +5,12 @@
 
 void ListaPratos(void)
 {
-    extern char nome[10][50];
+    //Utilizado o extern para acessar as variáveis globais criadas
+    extern char pratos[10][50];
     extern char descricao[10][300];
     extern float preco[10];
 
-    int tamanhoArrayNome = sizeof(nome) / sizeof(nome[0]);
+    int tamanhoArrayNome = sizeof(pratos) / sizeof(pratos[0]); //Faz com que o tamanho da array seja variável, dependendo de quantos pratos existe
 
     printf("\n");
     printf("*************\n");
@@ -17,13 +18,15 @@ void ListaPratos(void)
     printf("*************\n");
     printf("\n");
 
+    //Loop de repetição para apresentar cada caracatere de cada linha e cada coluna dos nossos vetores
     for (int i = 1; i < tamanhoArrayNome; i++)
     {
+        if (strlen(pratos[i]) != 0){ //Verificação se existe algo naquela posição, caso verdadeiro é mostrado o conteúdo
         printf("PRATO NUMERO %d\n", (i));
-        printf("Nome do prato %d: %s\n", (i), nome[i]);
+        printf("Nome do prato %d: %s\n", (i), pratos[i]);
         printf("Descricao do prato %d: %s\n", (i), descricao[i]);
         printf("Preco do prato %d: %.2f\n", (i), preco[i]);
         printf("--------------------------------------\n");
-
+        }
     }
 }
