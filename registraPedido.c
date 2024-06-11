0faa465dc367bd893c75f4de7adc078178b35b5c
Gabriel Ceron Bianchi <142252182+cbGabriel9@users.noreply.github.com>
2024-06-10 21:50:01 -0300

    Ajuste final - Comentários

--- registraPedido.c
+++ registraPedido.c
@@ -3,30 +3,31 @@
 #include <string.h>
 #include <stdbool.h>
 
-void limpaBufferDeEntrada() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) { }
-}
-
 void RegistraPedido(void) {
+
+    //Utilizado o extern para acessar as variáveis globais criadas
+    extern char pratos[10][50];
+    extern float preco[10];
+    int c;
+
     printf("\n");
     printf("*******************\n");
     printf("REGISTRO DE PEDIDOS\n");
     printf("*******************\n");
     printf("\n");
 
-    printf("Aperte enter para listar os pratos cadastrados\n");
-    limpaBufferDeEntrada();  
+    printf("Aperte enter para listar os pratos cadastrados\n"); 
+    while ((c = getchar()) != '\n' && c != EOF) { }  //Apagar o caractere de nova linha (\n) para que não interfira no getchar
     getchar();
 
-    ListaPratos();
+    ListaPratos(); //Chama a função de listagem já criada em outro arquivo
 
     bool loop = true;
     int quantidadeTotalDePratos = 0;
     char pratosPedidos[10][50];  
     int quantidadesEscolhidas[10] = {0};  
-
     float precoTotalPedido = 0;
+
     do {
         int pratoEscolhido = 0;
         int quantidade = 0;
@@ -34,17 +35,19 @@ void RegistraPedido(void) {
         printf("Digite o numero do prato escolhido: ");
         scanf("%d", &pratoEscolhido);
 
-        limpaBufferDeEntrada();
+        limpaBufferDeEntrada(); //Chama a função para limpar o caractere de nova linha
 
         printf("Quantos desse prato serao pedidos? (Digite apenas o numero): ");
         scanf("%d", &quantidade);
         limpaBufferDeEntrada();
 
-        strcpy(pratosPedidos[quantidadeTotalDePratos], nome[pratoEscolhido]);
+        /*Quando o usuário digitar o número do prato, a posição do array escolhida vai ser copiada para o novo array de pedidos(quantidadeTotaldePratos),
+        fazendo assim uma nova lista de pratos, mas apenas com o que o usuário escolheu e a quantidade de cada */
+        strcpy(pratosPedidos[quantidadeTotalDePratos], pratos[pratoEscolhido]); 
         quantidadesEscolhidas[quantidadeTotalDePratos] = quantidade;
         quantidadeTotalDePratos++;
 
-        precoTotalPedido += preco[pratoEscolhido] * quantidade;
+        precoTotalPedido += preco[pratoEscolhido] * quantidade; //Faz o cálculo dos preços de cada prato e quantidade
 
         int decisao = 0;
         printf("Deseja adicionar mais um prato?\n");
@@ -58,6 +61,7 @@ void RegistraPedido(void) {
 
     } while (loop);
 
+    //A partir desta linha é mostrado o resultado de todas as funções e códigos acima (Nome dos pratos escolhidos, quantidade e preços totais)
     if (quantidadeTotalDePratos == 1)
         printf("Foi escolhido %d prato, sendo seu nome e quantidade: \n", quantidadeTotalDePratos);
     else
